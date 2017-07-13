7ffebd65641613bcda989b6567ee5d24c0b73aeb
ragavendra <ragavendra.bn@gmail.com>
2017-07-13 14:18:11 -0700

    code to remove relevant comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
 	def create
-		@article = Article.find(params[:article_id])
+		find_article
 		@comment = @article.comments.create(comment_params)
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		find_article
+		@comment = @article.comments.find(params[:id])
+		@comments.destroy
+		redirect_to article_path(@article)
+	end
+
+	def find_article
+		@article = Article.find(params[:article_id])
 	end
 
 	private
