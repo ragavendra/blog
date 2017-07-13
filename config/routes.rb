421b0a8a4b25c21aca794fb6c911c2ed6fd709be
ragavendra <ragavendra.bn@gmail.com>
2017-07-13 14:06:31 -0700

    added comments part

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #lets use set of objects which is resources
-  resources	:articles
+  resources :articles do
+	 resources :comments
+  end
 
   #set home page
   root	'welcome#index'
