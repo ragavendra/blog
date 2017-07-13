bd7e0231049fea436cba6049233648803870152e
ragavendra <ragavendra.bn@gmail.com>
2017-07-12 18:23:00 -0700

    created models for article save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #lets use set of objects which is resources
+  resources	:articles
+
+  #set home page
+  root	'welcome#index'
 end
